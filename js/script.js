bc82f8c54c7785866672f32f8752957651e4bc99
AleenAlhussain <devaleenalhussain@gmail.com>
2024-12-03 11:50:32 +0300

    build a store

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('.autoplay').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+
+    speed: 800,
+    prevArrow: '<span><i class="fa fa-angle-left leftArrow"></i></span>',
+    nextArrow: '<span><i class="fa fa-angle-right rightArrow"></i></span>',
+    autoplaySpeed: 2000,
+});
